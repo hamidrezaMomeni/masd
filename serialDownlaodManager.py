e3e60496602c47cf64f21ce0ce6dae4709dccec6
hamidrezaMomeni <momenih333@gmail.com>
2019-09-05 14:13:29 +0430

    upload version 1.1

--- serialDownlaodManager.py
+++ serialDownlaodManager.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 from os import system
 
 baseUrl = "http://mydiba.club"
-userMovie = input("Enter name Movie (Example harry potter): ")
+userMovie = input("Enter name Movie (Example the flash): ")
 nameMovie = userMovie.replace(" ", "+")
 searchUrl = baseUrl + "/?s="+str(nameMovie)
 searchPage = requests.get(searchUrl)
@@ -25,21 +25,22 @@ for item in div:
    print(str(counter) + "){}".format(item.text))
    counter += 1
 
-userSelected = int(input("enter number of movie: ")) - 1
+userSelected = int(input("Select movie number: ")) - 1
 movieSelected = div[userSelected]
 if "سریال" in movieSelected.text.strip():
     movieUrl = movieSelected.find("a")["href"]
     moviePage = requests.get(movieUrl)
     
     soup = BeautifulSoup(moviePage.content, "html5lib")
-    divBody = soup.find_all("div",{"class":"-dl"})
+    # divDl = soup.find_all("div",{"class":"-dl"})
     title = soup.find_all("h4",{"class":"-dl-title"})
+    divBody = soup.find_all("div",{"class":"-dl-body"})
     counter = 1
     for item in title:
         print(str(counter) + ") {}".format(item.text.strip()) + "\n")
         counter += 1
         
-    userChoose = int(input("enter number of your coose: ")) - 1
+    userChoose = int(input("choose one: ")) - 1
     insideTitle = title[userChoose].text.strip() 
     userSelected = divBody[userChoose]
     blockLinks = userSelected.find("div",{"class":"Block_links"})
@@ -49,9 +50,8 @@ if "سریال" in movieSelected.text.strip():
         print(str(counter) + ") " + item.text)
         print()
         counter += 1
-    partSelected = int(input("Enter number of part movie: ")) - 1
+    partSelected = int(input("Enter part movie: ")) - 1
     urlDownload = movieLinks[partSelected]["href"]
-    
     system("mkdir -p '{}' && cd '{}' && wget '{}'".format(nameMovie,nameMovie,urlDownload))
     
     
